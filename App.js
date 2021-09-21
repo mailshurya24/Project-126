3b97b082c4ace8716f065608c1ec6181938cd9ab
mailshurya24 <70964703+mailshurya24@users.noreply.github.com>
2021-09-21 21:41:16 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import CameraPage from './Camera.js';
+
+export default class App extends React.Component {
+    render() {
+        return (
+            <CameraPage />
+        );
+    };
+};
